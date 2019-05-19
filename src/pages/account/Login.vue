96d82543854e1516396a4bee8d645000f889e0c6
diguifi <lumia-diguifi@hotmail.com>
2019-05-19 14:46:13 -0300

    Fixes sizes and responsivity on login page

--- src/pages/account/Login.vue
+++ src/pages/account/Login.vue
@@ -1,38 +1,39 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-form
-        @submit="onSubmit"
-        class="q-gutter-lg loginForm">
+  <q-page class="row text-center">
+    <div class="col-3"></div>
+    <div class="col self-center">
+      <q-form
+          @submit="onSubmit"
+          class="q-gutter-lg">
 
-        <h1>Abrantos</h1>
+          <h3>Abrantos</h3>
 
-        <q-input
-          filled
-          v-model="username"
-          label="Username"
-          hint="Digite seu nome de usuário"
-          lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Campo obrigatório']" />
+          <q-input
+            filled
+            v-model="username"
+            label="Username"
+            hint="Digite seu nome de usuário"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Campo obrigatório']" />
 
-        <q-input
-          v-model="password"
-          filled
-          type="password"
-          label="Senha"
-          hint="Digite sua senha"
-          lazy-rules
-          :rules="[ val => val && val.length > 0 || 'Campo obrigatório']" />
+          <q-input
+            v-model="password"
+            filled
+            type="password"
+            label="Senha"
+            hint="Digite sua senha"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Campo obrigatório']" />
 
-        <div>
-          <q-btn label="Login" type="submit" color="primary"></q-btn>
-          <q-btn label="Registrar" @click="onRegister()" color="primary" flat class="q-ml-sm" />
-        </div>
-      </q-form>
+          <div>
+            <q-btn label="Login" type="submit" color="primary"></q-btn>
+            <q-btn label="Registrar" @click="onRegister()" color="primary" flat class="q-ml-sm" />
+          </div>
+        </q-form>
+      </div>
+      <div class="col-3"></div>
   </q-page>
 </template>
-
-<style>
-</style>
 
 <script>
 export default {
@@ -66,9 +67,3 @@ export default {
   },
 };
 </script>
-
-<style>
-  .loginForm {
-    width: 50%;
-  }
-</style>
