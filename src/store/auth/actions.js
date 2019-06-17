6596270e9437cc94479fcd58a3b47e64b49f5eb2
diguifi <lumia-diguifi@hotmail.com>
2019-06-16 22:32:51 -0300

    Fix login params

--- src/store/auth/actions.js
+++ src/store/auth/actions.js
@@ -3,7 +3,7 @@ import Api from '../../api';
 
 export async function AUTH_REQUEST(context, loginParams) {
   const api = new Api();
-  const response = await api.post('authentication/login', { userName: loginParams.username, password: loginParams.password });
+  const response = await api.post('authentication/login', { userNameOrEmail: loginParams.username, password: loginParams.password });
 
   const token = response.data.accessToken;
   localStorage.setItem('abrantostoken', token);
